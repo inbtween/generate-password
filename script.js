627fa2e120e1fa3d9ebc5fe7265b272cc0292316
Michelle Smith <smith.michelle.a@gmail.com>
2021-01-14 19:08:28 -0500

    added password script

---
+++ script.js
@@ -0,0 +1,41 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(8);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(len) {
+  var length = len ? len : 10;
+  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper
+  var numeric = "0123456789";
+  var punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  var password = "";
+  var character = "";
+  var crunch = true;
+  while (password.length < length) {
+    entity1 = Math.ceil(string.length * Math.random() * Math.random());
+    entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
+    entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
+    hold = string.charAt(entity1);
+    hold = password.length % 2 == 0 ? hold.toUpperCase() : hold;
+    character += hold;
+    character += numeric.charAt(entity2);
+    character += punctuation.charAt(entity3);
+    password = character;
+  }
+  password = password
+    .split("")
+    .sort(function () {
+      return 0.5 - Math.random();
+    })
+    .join("");
+  return password.substr(0, len);
+}
